ca0bc58dca86c0e6da07f40e31c44e833b8ff58d
Siul Gutierrez [rootCrash] <siul.gomez94@gmail.com>
2019-07-29 13:37:01 -0500

    Hide to keylogger window and prevent it from popping

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    MSG Msg;
+    while(GetMessage(&Msg, NULL, 0, 0)) {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
     return 0;
 }
